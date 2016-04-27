4d59303278dc236afe8df1bdbee83804ae0b5348
git@some.com <git@some.com>
2016-04-27 21:08:52 +0800

    fix bugs.

--- app/test/TestApk.scala
+++ app/test/TestApk.scala
@@ -8,10 +8,13 @@ import com.google.gson.{GsonBuilder, Gson}
  *  15-8-7.
  */
 object TestApk {
+
   def mainTestApk(args: Array[String]) {
     val appInfo=ReadApkPackageInfo.getApkInfo("./Q2hBcHBTdG9yZV92MS4wLjdfMTE0Mzg5MzUyNjI1MjU=.apk")
     val gson=new GsonBuilder().create()
     println(gson.toJson(appInfo))
     println(appInfo.getDevcode+Parser.ObjectToJsonString(appInfo)+appInfo.getVersioncode+appInfo.getApkPackage)
   }
+
+
 }
